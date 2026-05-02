61f0c0260badc8b14986c0b446843e805cb7d501
umar abdullah <146557884+umar952@users.noreply.github.com>
2026-05-02 18:32:34 +0600

    js
    js files

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("[src='images/4.jpg']").hover(function(){
+        $(this).animate({
+            "hight":"+20px"
+        },2000)
+   
+    });
+
+    // let umar="<h1>hello</h1>";
+
+    $(".about-us[class='img']").append({
+        
+    });
+
+});
